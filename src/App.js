5045dec7ae77b7979930cce18bedfa1bf0ea9a76
Ryan Hung <ryanhungryan@gmail.com>
2024-04-13 19:59:42 +0100

    integrate react three fiber into the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-
+import { FiberContainer } from "./FiberContainer";
 function App() {
   return (
     <div className="App">
-      <h1> ghibli-style-shader </h1>
+      <FiberContainer />
     </div>
   );
 }
